eb0cc3ec78c041d94d0c2903f839a8c6c8d0326d
Przemysław Górecki <gorecki@ermlab.com>
2014-08-28 15:50:13 +0200

    Update to Meteor 0.9.x

--- package.js
+++ package.js
@@ -1,10 +1,13 @@
 Package.describe({
-    summary: "Gitlab for meteor"
+    summary: "Gitlab for meteor",
+  version: "0.1.1",
+  git: "https://github.com/Ermlab/meteor-nxt-gitlab.git"
 });
 
 Npm.depends({"nxt-gitlab":"0.9.1"});
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
     api.export("GitLab");
     api.add_files("gitlab.js", "server");
 });
